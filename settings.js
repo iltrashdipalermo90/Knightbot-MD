68ef31872913e5a3a4e4596af7cbe8fbb30f5440
iltrashdipalermo90 <iltrashdipalermo9090@gmail.com>
2025-04-07 02:25:06 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'iltrashdipalermo',
+  author: 'iltrashdipalermo',
+  botName: "Trash Bot",
+  botOwner: 'iltrashdipalermo', // iltrashdipalermo
+  ownerNumber: '18033035111,48699531070', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
